5abbef7daed19cd850f2bc5a631a9a6992b9fa6a
Daniyal <daniyalsjd@gmail.com>
2026-01-12 07:09:58 +0500

    chore: Final workspace cleanup and .gitignore fix

--- debug_build.js
+++
@@ -1,8 +0,0 @@
-import { exec } from 'child_process';
-import { writeFileSync } from 'fs';
-
-exec('npm run build', { cwd: './blog' }, (error, stdout, stderr) => {
-    const output = `STDOUT:\n${stdout}\n\nSTDERR:\n${stderr}\n\nERROR:\n${error}`;
-    writeFileSync('build_debug.log', output);
-    console.log('Build output captured to build_debug.log');
-});
